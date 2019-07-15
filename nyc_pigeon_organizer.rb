2bfda818e65dd94fd91736e954381001d225b6e0
Cody Shaben <cshaben87@gmail.com>
2019-07-15 02:26:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,5 @@
-def nyc_pigeon_organizer(data)
-  pigeon_data.reduce({}) do |memo,pair|
+def nyc_pigeon_organizer(pigeon_data)
+  pigeon_name.reduce({}) do |memo,pair|
     p memo
     p pair
     memo
